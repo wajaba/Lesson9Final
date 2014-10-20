6e4005162c5a8a2c80350e44563ccebc70a01e5c
tann1260 <tann1260@ISS147D8-27.scdsb.pri>
2014-10-20 08:31:36 -0400

    Final piece, running bug tests.

---
+++ Lesson8/src/lesson8/Lesson8.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lesson8;
+
+/**
+ *
+ * @author tann1260
+ */
+public class Lesson8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
