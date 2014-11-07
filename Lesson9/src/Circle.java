a108c05f17888733079846db5990b2a5a343dc13
tann1260 <tann1260@ISS147D8-27.scdsb.pri>
2014-11-07 09:12:33 -0500

    Initial Commit.

---
+++ Lesson9/src/Circle.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author tann1260
+ */
+public class Circle implements Shape{
+    public double area() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+    
+}
---
+++ Lesson9/src/Shape.java
@@ -0,0 +1,7 @@
+
+import TurtleGraphics.*;
+
+public interface Shape {
+
+    public double area();
+}
